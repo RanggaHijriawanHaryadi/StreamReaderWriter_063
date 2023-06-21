6860e586e015b47e7b033e1588ddecbcd730da6d
RanggaHijriawaHaryadi <rangghrydi04@gmail.com>
2023-06-21 10:34:37 +0700

    melakukan pilihan dengan while true dengan ada pemasukan dari cin, serta baris

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -11,4 +11,18 @@ int main() {
 	outfile.open("contohfile.txt");
 
 	cout << ">= Menulis file, \'q\q untuk keluar" << endl;
+
+	//inlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika memasukan karakter q
+		if (baris == "q") break;
+		// menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+
+
+	
 }
